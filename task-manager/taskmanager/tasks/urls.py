e47679e1dcd13dd923f6d7f070eeef121ba2d9d5
yadavaman977 <amankumaryadav2062@gmail.com>
2026-04-07 07:06:50 +0545

    Updated project files

--- task-manager/taskmanager/tasks/urls.py
+++ task-manager/taskmanager/tasks/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.home, name='home'),
+    path('', views.task_list, name='task_list'),
+    path('task/<int:pk>/', views.task_detail, name='task_detail'),
 ]
